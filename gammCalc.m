ba28fb8a0c84e4e37ed7f0188b59505715822483
Markus Berthilsson <Markus.Berthilsson@gmail.com>
2022-11-09 11:52:57 +0100

    Missed a file

--- gammCalc.m
+++
@@ -1,8 +0,0 @@
-function [gamma] = gammCalc(inputMatrix)
-%gammCalc Calculates the imaginary values of a vector
-%   Detailed explanation goes here
-gamma = zeros(size(inputMatrix));
-for R = 1:size(inputMatrix,3)
-    gamma(:,:,R) = real(1i * (inputMatrix(:,:,R) - inputMatrix(:,:,R)'));
-end
-
